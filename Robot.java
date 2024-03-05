715e7b9cbdecefe36775d976f06e55261a899de6
kpnssbv <kpns.sreeram@bazaarvoice.com>
2024-03-05 10:19:06 +0530

    Add method - Add static method for algorithm

--- Robot.java
+++ Robot.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
-public class Robot{
+public class Robot {
+    static void finalPosition(String move) {
+       
+    }
+
     public static void main(String[] args) {
-        
+        String move = "UDDLR";
+        finalPosition(move);
     }
 }
